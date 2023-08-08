c9970bdef3149128779b811e14a08f5c501fb594
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-08-08 18:09:26 +0530

    Create Logical Operators.cpp

---
+++ Logical Operators.cpp
@@ -0,0 +1,11 @@
+
+void logicOp(int a, int b)
+{
+    /*output (a&&b), (a||b), and ((!a)&&(!b))separated by spaces*/ 
+    cout<<(a&&b);
+    cout<<" ";
+    cout<<(a||b);
+    cout<<" ";
+    cout<<((!a)&&(!b));
+    cout<<endl;
+}
